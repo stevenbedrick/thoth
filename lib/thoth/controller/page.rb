467d66c47506f3270ff6c97413400c1fb777266c
Steven Bedrick <steve@bedrick.org>
2009-02-09 09:40:27 -0800

    Removed stray ruby-debug require

--- lib/thoth/controller/page.rb
+++ lib/thoth/controller/page.rb
@@ -25,7 +25,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #++
-require 'ruby-debug'
+
 module Thoth
   class PageController < Ramaze::Controller
     map       '/page'
